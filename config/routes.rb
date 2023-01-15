83042f4ea1cd9b68df681cd9bd58f4a14acca108
EC2 Default User <ec2-user@ip-172-31-35-40.ap-northeast-1.compute.internal>
2023-01-15 15:34:08 +0000

    [0115]実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root to: "homes#top"
+  
   #devise_for :admins
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
@@ -10,5 +10,39 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+  namespace :admin do
+    root to: "homes#top"
+    
+    resources :items,only: [:index, :new, :create, :show, :edit, :update]
+    
+    resources :genres,only: [:index, :create, :edit, :update]
+    
+    resources :customers,only: [:index, :show, :edit, :update]
+    
+    resources :orders,only: [:show, :update]
+    
+    resources :order_details,only: [:update]
+  end
+  
+  scope module: :public do
+    root to: "homes#top"
+    get "about" => "homes#about"
+    resources :items,only: [:index, :show]
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/information/edit' => 'customers#edit'
+    patch 'customers/information' => 'customers#update'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    
+    resources :cart_items,only: [:index, :update, :destroy, :create]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    
+    resources :orders,only: [:new, :create, :index, :show]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/complete' => 'orders#complete'
+    
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
