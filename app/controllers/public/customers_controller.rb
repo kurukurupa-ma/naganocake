35a0532f62c10c786cc6338b47531c69692bee53
EC2 Default User <ec2-user@ip-172-31-35-40.ap-northeast-1.compute.internal>
2023-01-29 14:45:29 +0000

    [0129]編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,8 +1,25 @@
 class Public::CustomersController < ApplicationController
+  
+  before_action :authenticate_customer!
+  
   def show
+    @customer = Customer.find(params[:id])
+    if @customer.id != current_customer.id
+      redirect_to root_path
+    end
   end
   
   def edit
+    @customer = Customer.find(params[:id])
+    if @customer.id != current_customer.id
+      redirect_to root_path
+    end
+  end
+  
+  def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to root_path
   end
   
   def update
@@ -12,5 +29,10 @@ class Public::CustomersController < ApplicationController
   end
   
   def withdraw
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at)
   end
 end
