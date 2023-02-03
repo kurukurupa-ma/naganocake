fe885e356f1ccee349f931bdf5c4147f5d675ed3
EC2 Default User <ec2-user@ip-172-31-35-40.ap-northeast-1.compute.internal>
2023-02-03 14:15:00 +0000

    [0203]注文画面

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,6 @@ class CartItem < ApplicationRecord
   belongs_to :customer_params
   belongs_to :item_image
   
-  validate :count, presence
+  validate :amount, presence
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2023_01_23_144632) do
   end
 
   create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
     t.string "name", null: false
     t.string "postal_code", null: false
     t.string "address", null: false
@@ -60,12 +61,9 @@ ActiveRecord::Schema.define(version: 2023_01_23_144632) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "aitems", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "customer_id", null: false
     t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -98,6 +96,7 @@ ActiveRecord::Schema.define(version: 2023_01_23_144632) do
   end
 
   create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
@@ -107,6 +106,8 @@ ActiveRecord::Schema.define(version: 2023_01_23_144632) do
   end
 
   create_table "order_details", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "order_id", null: false
     t.integer "price", null: false
     t.integer "amount", null: false
     t.integer "making_status", default: 0, null: false
@@ -115,6 +116,7 @@ ActiveRecord::Schema.define(version: 2023_01_23_144632) do
   end
 
   create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "name", null: false
