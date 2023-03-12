1c334ca9f124173851e5895b8b1800090323b1d7
EC2 Default User <ec2-user@ip-172-31-35-40.ap-northeast-1.compute.internal>
2023-03-12 14:06:59 +0000

    [0312]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -17,6 +17,20 @@ class Public::OrdersController < ApplicationController
   end
   
   def create
+    @cart_item = CartItem.where(customer_id:[current_customer.id])
+    @order = Order.new(order_params)
+    @shipping_cost = 800
+    if params[:page] =="new"
+      render 'comfirm'
+    else
+      if @order.payment_method =="クレジットカード"
+        @order.order_status = 1
+      end
+      if @order.save
+        pcart_items.each do |cart_item|
+          OrderItem.create!(order_id: @order.id, amount:cart_item.amount, item_id:cart_item.item_id, price:cart_item.item.price)
+        end
+        
   end
   
   def index
